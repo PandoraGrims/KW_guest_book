1b9f70597f25a6b14edc23db37c9ff65c10a00c2
Pandora <nimfanefor@gmail.com>
2023-07-01 12:59:30 +0600

    Добавил BookAdmin админка джанго

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'created_at', 'updated_at', 'status']
+    list_display_links = ['id', 'author', 'email']
+    search_fields = ['author', 'description']
+    fields = ['author', 'email', 'description', 'created_at', 'updated_at', 'status']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Book, BookAdmin)
