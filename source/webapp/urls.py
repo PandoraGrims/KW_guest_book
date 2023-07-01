88c0a8ce9da2417afd1f39cb596305cdbed91f31
Pandora <nimfanefor@gmail.com>
2023-07-01 15:51:43 +0600

    Добавил возможность удаления и редактирования записей

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from webapp.views import book_list_view, book_create
+from webapp.views import book_list_view, book_create, book_update, task_delete
 
 urlpatterns = [
     path('', book_list_view, name="index"),
     path('book/add/', book_create, name="book_create"),
+    path('book/<int:pk>/update', book_update, name="book_update"),
+    path('book/<int:pk>/delete', task_delete, name="task_delete"),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import Book, status_choices
 from webapp.form import BookForm
@@ -24,17 +24,31 @@ def book_create(request):
         else:
             return render(request, "book_create.html", {"status_choices": status_choices, "form": form})
 
-#
-# def book_create(request):
-#     if request.method == "GET":
-#         form = BookForm()
-#         return render(request, "book_create.html", {"form": form})
-#     else:
-#         form = BookForm(data=request.POST)
-#         if form.is_valid():
-#             book = Book.objects.create(author=form.cleaned_data.get('author'),
-#                                        email=form.cleaned_data.get('email'),
-#                                        text=form.cleaned_data.get('text'))
-#             return redirect('index')
-#         else:
-#             return render(request, "index.html", {"form": form})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, id=pk)
+    if request.method == "GET":
+        form = BookForm(initial={"author": book.author,
+                                 "email": book.email,
+                                 "description": book.description,
+                                 })
+        return render(request, "book_update.html", {"status_choices": status_choices, "form": form})
+    else:
+        form = BookForm(data=request.POST)
+        if form.is_valid():
+            book.author = request.POST.get("author")
+            book.email = request.POST.get("email")
+            book.description = request.POST.get("description")
+            book.save()
+            return redirect("index")
+        else:
+            return render(request, "book_update.html", {"form": form})
+
+
+def task_delete(request, pk):
+    book = get_object_or_404(Book, id=pk)
+    if request.method == "GET":
+        return render(request, "delete_book.html", {"book": book})
+    else:
+        book.delete()
+        return redirect("index")
